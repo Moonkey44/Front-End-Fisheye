32937632ba688985d4117c8468170d97a89b389d
Melvin Marceaux <melvin.marceaux@gmail.com>
2022-07-26 08:30:52 +0200

    Importation des données json + renommage de mes dossiers

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,33 +1,14 @@
     async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        /*const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]*/
         const newRequest = new Request("../../data/photographers.json");
-        const photographers = await fetch(newRequest)
+        const photographersData = await fetch(newRequest)
             .then((response) => response.json())
-            .then((data) => console.log(data))
+            .then((data) => {
+                console.log(data);
+                return data.photographers
+            })
             .catch(console.error);        
         // et bien retourner le tableau photographers seulement une fois
-        /*return ({
-            photographers: [ 1, 2, 3]})*/
+        return (photographersData)
     }
 
     async function displayData(photographers) {
@@ -43,7 +24,7 @@
 
     async function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
+        const photographers = await getPhotographers();
         displayData(photographers);
     };
     
