14be80a33c5ed1e937c2be355e00e833a0e311cd
Melvin Marceaux <melvin.marceaux@gmail.com>
2022-07-26 03:36:03 +0200

    Ajout des assets complémentaires au site

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -30,6 +30,7 @@
 
         photographers.forEach((photographer) => {
             const photographerModel = photographerFactory(photographer);
+            console.log(photographerModel);
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
         });
