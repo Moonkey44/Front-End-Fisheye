58fc99d26ebd32d1f6d5fa1a71125c8e0f7b2ef9
Melvin Marceaux <melvin.marceaux@gmail.com>
2022-08-06 12:16:48 +0200

    Mise en forme de ma section pour mes photos et vidéo (css + js) + implémentation de la fonction getVideoOrPicture()

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,7 +1,24 @@
  function mediaFactory(data,name)
  {
-    const { id, photographerId, title, image, likes, date, price } = data;
-    const picture = `assets/photographers/Sample_Photos/${getUserFirstName(name)}/${image}`;
+    const { id, photographerId, title, image, video, likes, date, price } = data;
+
+    function getVideoOrPicture(image, video){
+      if(image === undefined){
+        const videoPath = `assets/photographers/Sample_Photos/${getUserFirstName(name)}/${video}`;
+        const videoElt = document.createElement("video");
+        videoElt.setAttribute("src", videoPath);
+        videoElt.setAttribute("alt", title);
+        videoElt.setAttribute("autoplay", "");
+        return videoElt; 
+      }
+      else{
+        const picturePath = `assets/photographers/Sample_Photos/${getUserFirstName(name)}/${image}`;
+        const pictureElt = document.createElement("img");
+        pictureElt.setAttribute("src", picturePath);
+        pictureElt.setAttribute("alt", title);
+        return pictureElt
+      }
+    }
 
     function getUserFirstName(name){
         const firstName = name.substring(0,name.indexOf(' '));
@@ -15,18 +32,26 @@
         const article = document.createElement("article");
         const figure = document.createElement("figure");
         const link = document.createElement("a");
-        const img = document.createElement("img");
         const figcaption = document.createElement("figcaption");
+        const divlikes = document.createElement("div");
+        const heart = document.createElement('i');
+        const pLikes = document.createElement('p');
         const h3 = document.createElement("h3");
-        img.setAttribute("src", picture);
-        img.setAttribute("alt", title);
-        link.appendChild(img);
+        heart.classList.add("fa-solid", "fa-heart");
+        heart.setAttribute("aria-label","likes");
+        pLikes.textContent = likes;
+        h3.textContent = title;
+        divlikes.setAttribute("aria-label","likes");
+        link.appendChild(getVideoOrPicture(image,video));
+        divlikes.appendChild(pLikes);
+        divlikes.appendChild(heart);
         figcaption.appendChild(h3);
+        figcaption.appendChild(divlikes);
         figure.appendChild(link);
         figure.appendChild(figcaption);
         article.appendChild(figure);
         return article;
     }
 
-    return {getUserFirstName, getPictureCardDOM, picture}
+    return {getUserFirstName, getPictureCardDOM}
  }
