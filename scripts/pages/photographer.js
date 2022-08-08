09b8d812e4e43828d2fbd0bc1717f1be5c67e029
Melvin Marceaux <melvin.marceaux@gmail.com>
2022-08-08 19:18:15 +0200

    Ajout d'une fonctionnalité de triage du bouton select + gestion du rafraichissement de la page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -7,6 +7,7 @@ function displayData(photographers, media)
     const urlsearchParams = new URLSearchParams(document.location.search);
     const photographerID = parseInt(urlsearchParams.get("id"));
     const picturesSection = document.querySelector(".pictures");
+    const mediaArray = [];
     photographers.forEach((photographer) => {
         if(photographer.id === photographerID){
             const photographerModel = photographerFactory(photographer);
@@ -14,8 +15,9 @@ function displayData(photographers, media)
             photographerModel.getUserRate();
             media.forEach((media) => {
                 if(photographerID === media.photographerId){
-                    const mediaModel = mediaFactory(media,photographerModel.name);
-                    picturesSection.appendChild(mediaModel.getPictureCardDOM());
+                    mediaArray.push(media);
+                    //const mediaModel = mediaFactory(media,photographerModel.name);
+                    //picturesSection.appendChild(mediaModel.getPictureCardDOM());
                 }
             })
         }
@@ -28,5 +30,7 @@ async function init(){
     const media = await getPhotographersOrMedia("media");
     displayData(photographers, media);
 }
+
+
 
 init();
