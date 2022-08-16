12719dcd7a3673dcb463da5d369bf5f92ab28004
Melvin Marceaux <melvin.marceaux@gmail.com>
2022-08-16 05:07:03 +0200

    gestion du reset de la modale + correction de l'input en textarea

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,12 +1,54 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
-    const body = document.querySelector("body");
+    modal.setAttribute("aria-hidden", false);
+    const main = document.querySelector("main");
+    const header = document.querySelector("header");
+    main.setAttribute("aria-hidden",true);
+    header.setAttribute("aria-hidden",true);
+    header.style.opacity = 0.6;
+    main.style.opacity = 0.6;
+    const firstInput = document.getElementById("first");
+    firstInput.focus();
+    document.addEventListener("keyup",function onKeyUp(e){
+        switch(e.key){
+            case "Escape":
+                closeModal();               
+                this.removeEventListener("keyup",onKeyUp);
+        }
+    });
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
+    modal.setAttribute("aria-hidden", true);
+    const main = document.querySelector("main");
+    const header = document.querySelector("header");
+    main.setAttribute("aria-hidden",false);
+    header.setAttribute("aria-hidden",false);
+    header.style.opacity = 1;
+    main.style.opacity = 1;
+    resetModal();
 }
 
+function returnListInput(){
+    const listInput = Array.from(document.querySelectorAll("input"));
+    const areaInput = document.getElementById("message");
+    listInput.push(areaInput);
+    return listInput;
+}
 
+function resetModal(){
+    returnListInput().forEach(function(input){
+        input.value ="";
+    });
+}
+
+function sendModal(event){
+    event.preventDefault();
+    returnListInput().forEach(function(input){
+        console.log(input.value);
+    });
+    closeModal();
+}
